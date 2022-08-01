810f361650ab9681e8c4dac95fca6633c30cdfce
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2022-08-02 00:56:50 +0530

    Added Migration

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from TelePort import app
+from TelePort import app,db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+manager.add_command('db',MigrateCommand)
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run(debug=True)
