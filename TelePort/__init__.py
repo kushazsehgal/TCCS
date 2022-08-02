6627191f36d6a281f4e3d8bf3427d58de89ea3ed
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2022-08-02 19:40:50 +0530

    Added migration

--- TelePort/__init__.py
+++ TelePort/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_session import Session
+from flask_migrate import Migrate
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TelePort.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///TelePort.db'
@@ -13,6 +14,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 bcrypt = Bcrypt(app)
 log_manage = LoginManager(app)
 from TelePort import routes
