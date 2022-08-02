cc6e4a034ed96b8e5fa12d9178153f2e63d47917
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2022-08-02 19:47:15 +0530

    Resolved postgres to postgresql

--- TelePort/__init__.py
+++ TelePort/__init__.py
@@ -8,7 +8,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TelePort.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///TelePort.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wixpnsmxofsmoo:b83766cfaa394bbd0dc88889caf7b6b7b38f947c4ef8d7142c5cb11c724c0906@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d8jkgtpjhv5fnm'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wixpnsmxofsmoo:b83766cfaa394bbd0dc88889caf7b6b7b38f947c4ef8d7142c5cb11c724c0906@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d8jkgtpjhv5fnm'
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
